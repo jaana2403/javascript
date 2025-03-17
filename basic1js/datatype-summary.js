d9bf62b54347885b1ba255ebb3641319a5aca1c4
jaana2403 <153086798+jaana2403@users.noreply.github.com>
2025-03-17 12:53:00 +0000

    more info abt data types

---
+++ basic1js/datatype-summary.js
@@ -0,0 +1,22 @@
+// primitive 
+/* 7 types - string , number , boolean , null , undefined , symbol (unique) , bigint
+
+reference type (non primitive type)
+array , object , functions 
+
+*/
+let outisedtem=null
+// let user;undefined 
+// symbol provide unqiness
+const id=Symbol('123')
+const idid=Symbol('123')
+console.log(id===idid)
+
+const hero=["shakitiman" ,"krish ","lal pari"];
+let myobj={
+    name:"ishita",
+    age:22,
+}
+const myfunc=function(){
+    console.log("hello world !")
+}
---
+++ basic1js/strnum.js
@@ -0,0 +1,12 @@
+// operations 
+let value = 3
+let neg=-value
+console.log(neg);
+console.log(2+2)
+let str1="ishu "
+let str2="janu "
+let str3= str1+str2
+console.log(str3)
+console.log("1"+2+2)
+// who is coming first will decide the data types 
+console.log(2+2+"5")
