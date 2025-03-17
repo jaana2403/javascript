b7fa24a3b2a1951acdde791e01b315b4ceb0d0e6
jaana2403 <153086798+jaana2403@users.noreply.github.com>
2025-03-17 15:52:04 +0000

    date

---
+++ basic1js/date.js
@@ -0,0 +1,10 @@
+let mydate= new Date()
+console.log(mydate.toString())
+console.log(mydate.toLocaleString())
+console.log(typeof mydate)
+
+let my= new Date(2023,0,23)
+console.log(my.toDateString())
+
+console.log(my.getTime())
+console.log(Date.now())
