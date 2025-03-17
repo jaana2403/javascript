a73c26296e9b2d1eebbda725b49dcda0ae1519f9
jaana2403 <153086798+jaana2403@users.noreply.github.com>
2025-03-17 12:29:48 +0000

    conversion of datatypes

---
+++ basic1js/conversion.js
@@ -0,0 +1,13 @@
+let score = 33
+// number 33=33
+// "33abc"= NaN 
+let islogin=1
+let booleanislogin=Boolean(islogin)
+console.log(booleanislogin)
+// 1 = true
+// "" = false 
+
+let m = 33
+let stringnum= String(m)
+console.log(stringnum)
+console.log(typeof stringnum)
