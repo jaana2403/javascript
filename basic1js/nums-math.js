9766432e28a2a7bb1e33cea12a7669cf88568ed3
jaana2403 <153086798+jaana2403@users.noreply.github.com>
2025-03-17 15:41:09 +0000

    math and memory

---
+++ basic1js/nums-math.js
@@ -0,0 +1,21 @@
+const score = 311
+console.log(score)
+const balance = new Number(100);
+// force fully number assigned 
+console.log(balance)
+console.log(balance.toString())
+console.log(balance.toFixed(2))
+
+const othernum=23.89764
+console.log(othernum.toPrecision(3))
+
+// mathssss 
+console.log(Math)
+console.log(Math.abs(-4))
+// .round (4.6) , .ceil() ,.floor() , .max , .min
+console.log(Math.random()) 
+// values btw 0 to 1 only
+
+const min =10;
+const max=20
+console.log(Math.floor(Math.random() * (max-min+1))+min)
---
+++ basic1js/stack-heap-memo.js
@@ -0,0 +1,12 @@
+// stack memory 
+// stack primitive memory 
+// heap - non primitive 
+const name = "ishita"
+const count=3
+console.log(`hello my name is  ${name} and my repo count is ${count}`);
+// charAt , indexOf , toUpperCase , substring (0,4) , slice(can take neg value also)
+const str="      ishu darling    "
+console.log(str.trim());
+// trim- extra spaces 
+
+
