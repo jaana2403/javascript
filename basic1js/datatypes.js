ff384282e8c1607e50fa6f2d167bf0767a8b047f
jaana2403 <153086798+jaana2403@users.noreply.github.com>
2025-03-17 12:17:30 +0000

    javascript learning

---
+++ basic1js/datatypes.js
@@ -0,0 +1,13 @@
+alert("hello")
+// we r using node js not browser 
+// ecma tc.es documentation of java script 
+let name="jahnavi rai "
+let age=15
+let islogin=true
+// number -> 2^53
+// string= ""
+// boolean= true/false 
+// null - > standalone value 
+// undefined -> 
+// symbol ->unique 
+console.log(typeof(24));
---
+++ basic1js/test.js
@@ -0,0 +1,11 @@
+console.log("jahnavi rai ")
+const accid=1245678
+// cannot update const variable
+var accname="ishita kumari "
+// scope based...perfer not to use var because of issue in block scope and fucntional scope 
+let accstate;
+let city="delhi"
+city="bengaluru"
+accname="ishu darling"
+// can update values of variables 
+console.table([accid,accname,city,accstate])
